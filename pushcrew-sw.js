61066536ed3facfc9fb6b25e2f53febb718a880d
nikhilkhangar <khangar.nikhil@gmail.com>
2018-06-19 15:58:42 +0530

    Update pushcrew-sw.js

--- pushcrew-sw.js
+++ pushcrew-sw.js
@@ -1,2 +1,2 @@
 var version = 6;
-importScripts('https://cdn.pushcrew.com/sw/61dd9a7239a4719e67b6d22a8c4e4e1f.js');
+importScripts('https://cdn.pushcrewstaging.com/sw/61dd9a7239a4719e67b6d22a8c4e4e1f.js');
